a717f63450565939be01cd164c3415601d9d12aa
QuocVo87 <quocvo87@gmail.com>
2024-01-04 15:14:23 +0700

    call mapping in chid composer

--- src/Child.php
+++ src/Child.php
@@ -1,10 +1,19 @@
 <?php
 namespace ChildComposer;
 
+use TrueMe\MapArr;
+
 class Child
 {
     public static function process()
     {
         var_dump('This is ChildComposer Child and process');
+    }
+
+    public static function callMapping()
+    {
+        $mapping = new MapArr;
+
+        var_dump("Child composer call mapping", $mapping);
     }
 }
